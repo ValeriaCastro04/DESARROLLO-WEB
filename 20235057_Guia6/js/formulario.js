3cedf4d5fc67dbcc134a74f5bdf98a5a618f1c08
ValeriaCastro04 <vpcastro344@gmail.com>
2024-10-28 17:57:06 -0600

    Update formulario.js

---
+++ 20235057_Guia6/js/formulario.js
@@ -0,0 +1,124 @@
+//accediendo a los elementos html
+const inputNombre = document.getElementById("idTxtNombre");
+const inputApellido = document.getElementById("idTxtApellido");
+const inputFechaNacimiento = document.getElementById("idTxtFechaNacimiento");
+const inputRdMasculino = document.getElementById("idRdMasculino");
+const inputRdFemenino = document.getElementById("idRdFemenino");
+const cmbPais = document.getElementById("idCmbPais");
+const inputDireccion = document.getElementById("idTxtDireccion");
+const inputNombrePais = document.getElementById("idNombrePais");
+
+const buttonAgregarPaciente = document.getElementById("idBtnAgregar");
+const buttonLimpiarPaciente = document.getElementById("idBtnLimpiar");
+const buttonMostrarPaciente = document.getElementById("idBtnMostrar");
+const buttonAgregarPais = document.getElementById("idBtnAddPais");
+
+const notificacion = document.getElementById("idNotificacion");
+//componente de bootstrap
+const toast = new bootstrap.Toast(notificacion);
+const mensaje = document.getElementById("idMensaje");
+
+//componente modal
+const idModal = document.getElementById("idModal");
+
+//arreglo global de paciente
+let arrayPaciente = [];
+
+//creando funcion para que se limpie el formulario
+const limpiarForm = () =>{
+    inputNombre.value="";
+    inputApellido.value="";
+    inputFechaNacimiento.value="";
+    inputRdMasculino.checked=false;
+    inputRdFemenino.checked=false;
+    cmbPais.value = 0;
+    inputDireccion.value = "";
+    inputNombrePais.value="";
+
+    inputNombre.focus();
+};
+//funcion para validar el ingreso del paciente
+const addPaciente = function(){
+    let nombre = inputNombre.value;
+    let apellido = inputApellido.value;
+    let fechaNacimiento = inputFechaNacimiento.value;
+    let sexo =
+        inputRdMasculino.checked == true
+        ? "Hombre"
+        : inputRdFemenino.checked == true
+        ? "Mujer"
+        : "";
+    let pais = cmbPais.value;
+    let labelPais = cmbPais.options[cmbPais.selectedIndex].text;
+    let direccion = inputDireccion.value;
+
+    if (
+        nombre != "" &&
+        apellido != "" &&
+        fechaNacimiento != "" &&
+        sexo != "" &&
+        pais != 0 &&
+        direccion != ""
+    ) {
+        arrayPaciente.push(
+            new Array(nombre, apellido, fechaNacimiento, sexo, labelPais, direccion)
+        );
+
+        mensaje.innerHTML = "Se ha registrado un nuevo paciente";
+
+        toast.show();
+
+        limpiarForm();
+    } else {
+        mensaje.innerHTML = "Faltan campos por completar";
+
+        toast.show();
+    }
+};
+
+function imprimirFilas(){
+    let $fila = "";
+    let contador = 1;
+
+    arrayPaciente.forEach((element) =>{
+        $fila +=`<tr>
+                    <td scope="row" class="text-center fw-bold">${contador}</td>
+                    <td>${element[0]}</td>
+                    <td>${element[1]}</td>
+                    <td>${element[2]}</td>
+                    <td>${element[3]}</td>
+                    <td>${element[4]}</td>
+                    <td>${element[5]}</td>
+                    <td>
+                        <button id="idBtnEditar${contador}" type="button" class="btn btn-primary" alt="Eliminar">
+                            <i class="bi bi-pencil-square"></i>
+                        </button>
+                        <button id="idBtnEliminar${contador}" type="button" class="btn btn-danger" alt="Editar">
+                            <i class="bi bi-trash3-fill"></i>
+                        </button>
+                    </td>
+                </tr>`;
+        contador++;
+    });
+    return $fila;
+}
+
+const imprimirPacientes = () => {
+    let $table = `<div class="table-responsive">
+                    <table class="table table-striped table-hover table-bordered">
+                        <tr>
+                            <th scope="col" class="text-center" style="width:5%">#</th>
+                            <th scope="col" class="text-center" style="width:15%">Nombre</th>
+                            <th scope="col" class="text-center" style="width:15%">Apellido</th>
+                            <th scope="col" class="text-center" style="width:10%">Fecha nacimiento</th>
+                            <th scope="col" class="text-center" style="width:10%">Sexo</th>
+                            <th scope="col" class="text-center" style="width:10%">#Pais</th>
+                            <th scope="col" class="text-center" style="width:25%">#Direccion</th>
+                            <th scope="col" class="text-center" style="width:10%">Opciones</th>
+                        </tr>
+                        ${imprimirFilas()}
+                    </table>
+                </div>
+    `;
+    document.getElementById("idTablaPacientes").innerHTML = $table;
+}
